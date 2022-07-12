3ccf0c4d635821b77154ddb8ffb827feb0a49d66
darkrabel <faheemeducation2020@gmail.com>
2022-07-12 17:21:05 +0000

    upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+print("hello world");
+name = "faheem";
+print(name)
+age = 16;
+print(age);
+
+age = age+1;#after this age will be print as 17;
+
+print(age);
+is_adult = False;
+print(is_adult);
---
+++ exersise2.py
@@ -0,0 +1,3 @@
+Secretsuperhero = input("what is your favourite superhero?")
+name = "Tony"
+print(name + " your secret superhero is "+Secretsuperhero)
