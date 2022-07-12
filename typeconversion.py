7b0bfae56409c3bf055a090b147cc7b86296b455
darkrabel <faheemeducation2020@gmail.com>
2022-07-12 17:34:17 +0000

    leaving at 21:30

---
+++ typeconversion.py
@@ -0,0 +1,11 @@
+old_age = input("what is ur age : ")#by defualt it will be string 
+
+new_age = int(old_age)+2;#converting string to integer
+
+print(new_age);
+#other funtions like int() functions
+#
+#float()
+#str()
+#bool()
+#leaveing at 21:30
