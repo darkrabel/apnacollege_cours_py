663877c2b73bba3480dad165dc120fc77937381c
faheem45 <79135048+darkrabel@users.noreply.github.com>
2022-07-19 10:36:05 +0000

    after making calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num1 = input("enter the first number ")
+num2 = input("enter the second number")
+
+
+
+print(float(num1)+float(num2));
+print(float(num1)-float(num2));
+print(float(num1)*float(num2));
+
+print(float(num1)/float(num2));
+print(float(num1)**float(num2));
+print(float(num1)%float(num2));
